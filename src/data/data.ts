eddd3ad7d49a830b335ddc8f08ddfdd159e4527a
JB <jeanbernard_doffemont@yahoo.fr>
2022-03-27 18:13:05 +0200

    commit data json

---
+++ src/data/data.ts
@@ -0,0 +1,32 @@
+export const data = [
+    {
+        avatar: "../public/avatar.jpg",
+        nom: "Duprès",
+        prenom: "Louis",
+        message: "Content de t'avoir revu...",
+    },
+    {
+        avatar: "../public/avatar.jpg",
+        nom: "Carpentier",
+        prenom: "Mélanie",
+        message: "Ok pour demain, je prend ce qu'il faut !",
+    },
+    {
+        avatar: "../public/avatar.jpg",
+        nom: "Huet",
+        prenom: "Simon",
+        message: "Merci :)",
+    },
+    {
+        avatar: "../public/avatar.jpg",
+        nom: "Faure",
+        prenom: "Fauve",
+        message: "Tu as vu la dernière saison de GOT ?",
+    },
+    {
+        avatar: "../public/avatar.jpg",
+        nom: "Leger",
+        prenom: "Louise",
+        message: "Je pars pour Paris dans 4 heures",
+    },
+];
