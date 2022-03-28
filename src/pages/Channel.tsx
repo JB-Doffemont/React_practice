6618695bc8150b53b9473d5d685829b220d72a33
JB <jeanbernard_doffemont@yahoo.fr>
2022-03-28 14:52:53 +0200

    commit display length array

--- src/pages/Channel.tsx
+++ src/pages/Channel.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import AddBtn from '../components/AddBtn';
 import Contact from '../components/Contact';
+import contactData from '../data/contactData';
 
 const Channel = () => {
     return (
         <div className="main_container">
+            
         <div className='main_top'>
             <div className="main_title">
-                <h1>Mes discussions</h1>
+                <h1>Mes discussions</h1>  <p>({contactData.length})</p>
             </div>
+            
+            
             <div className="main_addBtn">
                 <AddBtn></AddBtn>
             </div>
