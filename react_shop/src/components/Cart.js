5003ee6162c3b425e2936230ce266e3e6aef99b3
JB <jeanbernard_doffemont@yahoo.fr>
2022-08-21 15:37:53 +0200

    cart style

--- react_shop/src/components/Cart.js
+++ react_shop/src/components/Cart.js
@@ -1,10 +1,12 @@
+import {} from "../styles/cart.css";
+
 function Cart() {
   const monsteraPrice = 8;
   const ivyPrice = 10;
   const flowerPrice = 15;
   return (
-    <div>
-      <h2>Pannier</h2>
+    <div className="lmj-cart">
+      <h2>Panier</h2>
       <ul>
         <li>Monstera : {monsteraPrice}€</li>
         <li>Lierre : {ivyPrice}€</li>
