5c54a5c5ecaa16b52ee852483622a28679b27e7f
JB <jeanbernard_doffemont@yahoo.fr>
2022-08-21 15:32:18 +0200

    logo + style

--- react_shop/src/components/Banner.js
+++ react_shop/src/components/Banner.js
@@ -1,9 +1,16 @@
 import "../styles/banner.css";
+import logo from "../assets/logo.png";
 
 function Banner() {
+  const title = "La maison de la jungle";
   return (
     <div className="lmj-banner">
-      <h1>La maison jungle</h1>
+      <img
+        src={logo}
+        alt="Logo feuille de la maison de la jungle"
+        className="lmj-logo"
+      />
+      <h1 className="lmj-title">{title}</h1>
     </div>
   );
 }
